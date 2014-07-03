a65623980788a162c43c8d778523addf47e24bc0
Leigh Maddock <lmaddock85@gmail.com>
2014-07-03 16:30:38 +1000

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# (C) 2014, Leigh Maddock, <awesomesourcesoftware@gmail.com>
+# Please see https://github.com/leighmaddock/Monitor-Agent/LICENSE for license informati
+on
 Monitor-Agent
 =============
 
@@ -6,3 +9,18 @@ A python based Remote Execution Agent.
 This was written as a lightweight python version of the NRPE agent.
 
 Description of each file below:
+monitor-agent.cfg
+---------
+This configuration file specifies which port an agent is listening for a given hostname. IP of said hostname can also be provided if using a monitoring tool that connects via IP instead (e.g. nagios). Specifying IP will not affect the agent operation.
+
+monitor-agent.init
+---------
+Basic init script that has been written to be as cross-OS as possible.
+
+monitor-agent.py
+---------
+Core python agent
+
+.monitor-agent.pass
+---------
+A password file which contains the commands the agent is allowed to run.
---
+++ monitor-agent.py
@@ -0,0 +1,85 @@
+# (C) 2014, Leigh Maddock, <awesomesourcesoftware@gmail.com>
+# Please see https://github.com/leighmaddock/Monitor-Agent/LICENSE for license informati
+on
+#!/usr/bin/env python
+#
+
+import SocketServer, subprocess, sys, os, signal
+from threading import Thread
+
+# listen on all interfaces
+IPADDR = ''
+cmdfile =  "/etc/.monitor-agent.pass"
+cfgfile =  "/etc/monitor-agent.cfg"
+
+################################################################################
+def usage():
+	sys.stderr.write("Usage: %s\n" % sys.argv[0])
+
+################################################################################
+def getPort():
+	# Just in case hostname contains domain info, split it.
+	hostname=os.uname()[1].split('.')[0]
+	# PORT will be default unless specifically mentioned in cfg
+	for line in open(cfgfile).readlines():
+		if line.strip() and not line.startswith("#"):
+			row = line.split()
+			if row[0] == hostname or row[0] == "default" and len(row) == 2:
+				PORT = int(row[1])
+	
+	return PORT
+
+################################################################################
+def checkCommand(password, commandname):
+	# Assume cmd is bad, unless we say it's good
+	validcmd = False
+	cmddata = open(cmdfile).readlines()
+	for line in cmddata:
+		row = line.split()
+		if password == row[0] and commandname == row[1]:
+			validcmd = True
+	
+	return validcmd
+
+################################################################################
+def pipe_command(args):
+	subp = subprocess.Popen(args, stdin=None, stdout=subprocess.PIPE)
+	return subp.communicate()[0]
+
+################################################################################
+class SingleTCPHandler(SocketServer.BaseRequestHandler):
+	def handle(self):
+		# self.request is the client connection 1kb limit
+		data = self.request.recv(1024)
+		row = data.split()
+		if checkCommand(row[0], row[1]):
+			reply = pipe_command(row[1:])
+		else:
+			reply = "Command %s not allowed to execute" % row[1]
+		if reply is not None:
+			self.request.send(reply)
+		self.request.close()
+
+################################################################################
+class SimpleServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+	daemon_threads = True
+	allow_reuse_address = True
+
+	def __init__(self, serveraddr, RequestHandlerClass):
+		SocketServer.TCPServer.__init__(self, serveraddr, RequestHandlerClass)
+
+################################################################################
+def main():
+	PORT = getPort()
+	server = SimpleServer((IPADDR, PORT), SingleTCPHandler)
+	try:
+		server.serve_forever()
+	except:
+		sys.exit(0)
+
+################################################################################
+if __name__=="__main__":
+	main()
+
+#EOF
+
